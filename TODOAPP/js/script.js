f0f7a3cbe20756e0ad80f6bf386bd64ab44c46e5
BrunoSimonDaSilva <96893269+BrunoSimonDaSilva@users.noreply.github.com>
2022-05-16 21:56:48 -0300

    Add files via upload

--- TODOAPP/js/script.js
+++ TODOAPP/js/script.js
@@ -27,17 +27,48 @@ let formValidation = ()=>{
     } else {
         console.log("Sucesso");
         msg2.innerHTML = "";
+        acceptData();
+        add.setAttribute("data-bs-dismiss", "modal");
+        add.click();
+
+        (()=>{
+            add.setAttribute("data-bs-dismiss", "modal");
+        })();
+        createTasks();
     }
 };
 
-let data = {};
+let data = [];
 
-let acceptData = ()=>{
-    data.push();
+let acceptData = ()=> {
+    data.push({
+        text: textInput.value,
+        date: dateInput.value,
+        description: textarea.value,
+    }); 
 
     localStorage.setItem("data",JSON.stringify(data));
     console.log(data);
 };
 
-
-    
+let createTasks = () => {
+    tasks.innerHTML = "";
+    data.map((x,y) => {
+        return (tasks.innerHTML +=`
+        <div id=${y}>
+            <span class="fw-bold">${x.text}</span>
+            <span class="small text-secondary">${x.date}</span>
+            <p>${x.description}</p>
+            <span class="options">
+                <i onClick="editTask(this)"
+                class="fas fa-edit" 
+                data-bs-toggle="model" 
+                data-bs-target="#form"></i>
+                <i onClick="deketeTask(this); createTask()"
+                class="fas fa-trash-alt"></i>
+            </span>
+        </div>
+        `
+        );
+    })
+}
